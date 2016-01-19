250f47dda08db28db53757e03e2704da374d7e73
Michael Fulton <mfulton.student@manhattan.edu>
2016-01-19 15:51:21 -0500

    Initial Commit

---
+++ SomeTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SomeTest
+//
+//  Created by Michael Fulton Jr. on 1/19/16.
+//  Copyright © 2016 Manhattan College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
